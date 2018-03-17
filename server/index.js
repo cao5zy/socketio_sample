a6c90fc3b20e27281f707fc73fe965b0ea1b352c
Ubuntu <caozon@dev.02pz4bqcczde1igra14ffnabtb.hx.internal.cloudapp.net>
2018-03-17 04:28:26 +0000

    format code

--- server/index.js
+++ server/index.js
@@ -1,30 +1,34 @@
-(function(factory){
+(function(factory) {
     module.exports = factory(require('http'), require('socket.io'), require('fs'));
-}(function(http, socket, fs){
-    function handler (req, res) {
-	fs.readFile(__dirname + '/index.html',
-		    function (err, data) {
-			if (err) {
-			    res.writeHead(500);
-			    return res.end('Error loading index.html');
-			}
+}(function(http, socket, fs) {
+    function handler(req, res) {
+        fs.readFile(__dirname + '/index.html',
+            function(err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
+                }
 
-			res.writeHead(200);
-			res.end(data);
-		    });
+                res.writeHead(200);
+                res.end(data);
+            });
     }
-    
-    (function(app){
-	(function(socketio){
-	    app.listen(8081);
-	    
-	    socketio.on("connection", function(skt){
-		skt.emit("news", {message: "connected!!! we can talk"});
 
-		skt.on("__message", function(data){
-		    skt.emit("messsage__", { message: data});
-		});
-	    });
-	}(socket(app)));
+    (function(app) {
+        (function(socketio) {
+            app.listen(8081);
+
+            socketio.on("connection", function(skt) {
+                skt.emit("news", {
+                    message: "connected!!! we can talk"
+                });
+
+                skt.on("__message", function(data) {
+                    skt.emit("messsage__", {
+                        message: data
+                    });
+                });
+            });
+        }(socket(app)));
     }(http.createServer(handler)));
 }));
